e7dea9c03f58b54094842eb9546708d981b4f629
AnkurOjha <ojhankur@gmail.com>
2024-08-27 13:39:36 +0530

    successful

--- src/weather_fetcher.py
+++ src/weather_fetcher.py
@@ -6,17 +6,17 @@ load_dotenv()
 
 API_KEY=os.getenv('API_KEY')
 BASE_URL=os.getenv('BASE_URL')
-CITY=os.getenv('CITY')
+CITY=input("Enter the city: ")
 
 def get_weather():
     params={
-        'q':input("Enter the City name: "),
+        'q':CITY,
         "appid":API_KEY,
         "units": 'metric'
     }
     response=requests.get(BASE_URL, params=params)
     data=response.json()
     if response.status_code==200:
-        return data['main']['temp'], data['weather'][0]['description']
+        return data['main']['temp'], data['weather'][0]['description'], CITY
     else:
         raise Exception(f"Error fetching weather data:{data['message']}")
